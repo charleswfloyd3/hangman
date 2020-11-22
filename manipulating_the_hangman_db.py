aae1545fac1a5b74406faf99352b4ff26fb90d67
Charles Floyd <71990660+charleswfloyd3@users.noreply.github.com>
2020-11-22 13:06:29 -0800

    Add files via upload
    This is a separate file(not needed to be run in conjunction with the game file)of the the SQL commands used in the hangman game to view all records and refresh the database. I created a separate file to store the SQL commands used, as well as to test out the creation of new features which incorporate SQL.

---
+++ manipulating_the_hangman_db.py
@@ -0,0 +1,29 @@
+import mysql.connector
+connection = mysql.connector.connect(
+       host="localhost",
+       user="root",
+       passwd="MyfirstDB33",
+       database="testDB"
+)
+cursor = connection.cursor(buffered=True)
+
+'''Deletes all records from all tables and creates a new starting point, basically a refresh on guess records'''
+tables = []
+for i in range(3,8):
+    tables.append(f"{str(i)}letter")
+for table in tables:
+     cursor.execute(f"TRUNCATE TABLE {table}")
+for table in tables:
+     cursor.execute(f"INSERT INTO {table} VALUES(0)")
+connection.commit()
+
+'''Retrieves all records from all tables'''
+tables = []
+for i in range(3,8):
+    tables.append(f"{str(i)}letter")
+for table in tables:
+    cursor.execute(f"SELECT * FROM {table}")
+    record = cursor.fetchone()[0]
+    print(f"{str(record)} is the record for {table} words.")
+
+connection.commit()
